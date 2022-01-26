afd078f424eaf06197fe330a976dc8d9e98a1699
fawriid <farid.miri.1393@gmail.com>
2022-01-26 13:29:18 +0330

    banner component created and styled and added to app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 import "./App.css";
 // components
 import Header from "./Components/Header";
+import Banner from "./Components/Banner";
 
 class App extends Component {
     render() {
         return (
             <>
                 <Header />
+                <Banner />
             </>
         );
     }
---
+++ src/Components/Banner.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import styles from "./Banner.module.css";
+import banner from "../images/banner.jpg";
+
+const Banner = () => {
+    return (
+        <>
+            <div className={styles.banner__container}>
+                <img className={styles.banner} src={banner} alt="banner"></img>
+                <div className={styles.text__banner}>
+                    <h2 className={styles.banner__title}>Digits.com</h2>
+                    <h5 className={styles.title__sub}>
+                        We're selling <span className={styles.selected}>Digitals</span>
+                    </h5>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Banner;
