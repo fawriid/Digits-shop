14f7c72718bddcbcb991c9565cd2c56fa0b2b735
fawriid <farid.miri.1393@gmail.com>
2022-02-03 14:32:10 +0330

    add and minus option for buying phone added

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
+import { ThemeConsumer } from "styled-components";
 
 import styles from "./Card.module.css";
 
 class Card extends Component {
+    constructor() {
+        super();
+        this.state = {
+            counter: 0,
+        };
+    }
+
+    up = () => {
+        this.setState((prevState) => ({
+            counter: prevState.counter + 1,
+        }));
+    };
+
+    down = () => {
+        if (this.state.counter > 0) {
+            this.setState((prevState) => ({
+                counter: prevState.counter - 1,
+            }));
+        }
+    };
+
     render() {
         const { image, name, price } = this.props;
 
@@ -10,9 +32,23 @@ class Card extends Component {
             <>
                 <div className={styles.container} id="products">
                     <img className={styles.img} src={image} alt="product"></img>
-                    <div className={styles.card__info}>
-                        <p className={styles.name}>{name}</p>
-                        <span className={styles.price}>{price}</span>
+                    <div className={styles.flex__container}>
+                        <div className={styles.card__info}>
+                            <p className={styles.name}>{name}</p>
+                            <span className={styles.price}>{price}</span>
+                        </div>
+                        <div className={styles.counter}>
+                            <span onClick={this.up}>
+                                <i className="fas fa-angle-up"></i>
+                            </span>
+                            <p>{this.state.counter}</p>
+                            <span
+                                onClick={this.down}
+                                className={this.state.counter ? "" : styles.disable}
+                            >
+                                <i className="fas fa-angle-down"></i>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </>
