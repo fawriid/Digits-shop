8585ded98afc9d375c6c65e24f2baa6c19852562
fawriid <farid.miri.1393@gmail.com>
2022-02-06 17:58:59 +0330

    changing cards component from hardcoded to dynamic with list rendering

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -10,14 +10,29 @@ import iphone12 from "../images/iphone12promax.jpg";
 import s21 from "../images/s21.jpg";
 
 class Cards extends Component {
+    constructor() {
+        super();
+        this.state = {
+            phoneData: [
+                { id: 1, image: iphonex, name: "Iphone X", price: "750 $" },
+                { id: 2, image: iphone11, name: "Iphone 11 pro max", price: "950 $" },
+                { id: 3, image: iphone12, name: "Iphone 12 pro max", price: "1100 $" },
+                { id: 4, image: s21, name: "S21 ultra", price: "990 $" },
+            ],
+        };
+    }
     render() {
         return (
             <>
                 <div className={styles.container}>
-                    <Card image={iphonex} name="Iphone X" price="750 $" />
-                    <Card image={iphone11} name="Iphone 11 pro max" price="950 $" />
-                    <Card image={iphone12} name="Iphone 12 pro max" price="1100 $" />
-                    <Card image={s21} name="S21 ultra" price="990 $" />
+                    {this.state.phoneData.map((phone) => (
+                        <Card
+                            key={phone.id}
+                            image={phone.image}
+                            name={phone.name}
+                            price={phone.price}
+                        />
+                    ))}
                 </div>
             </>
         );
